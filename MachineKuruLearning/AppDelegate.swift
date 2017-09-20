114717a1c03d60544f3782b917a7afd3e446ecc4
kuru <kurushetra5@gmail.com>
2017-09-21 00:41:47 +0200

    Initial Commit

---
+++ MachineKuruLearning/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MachineKuruLearning
+//
+//  Created by Kurushetra on 21/9/17.
+//  Copyright © 2017 Kurushetra. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MachineKuruLearning/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MachineKuruLearning
+//
+//  Created by Kurushetra on 21/9/17.
+//  Copyright © 2017 Kurushetra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
